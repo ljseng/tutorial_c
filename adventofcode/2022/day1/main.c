dae9b1560ec5879273d494b61d462741aac1cc3e
ljseng <sengleejun@gmail.com>
2023-10-01 03:04:48 +0800

    wip: Use singly linked list to track calories sum

--- adventofcode/2022/day1/main.c
+++ adventofcode/2022/day1/main.c
@@ -4,6 +4,42 @@
 
 #include "../../../dbgprint/debug_print.h"
 
+struct CaloriesSumNode {
+    int calories_sum;
+    struct CaloriesSumNode* next;
+};
+
+struct CaloriesSumNode* create_calories_sum_node(int calories_sum) {
+    struct CaloriesSumNode* new_node =  (struct CaloriesSumNode*)malloc(sizeof(struct CaloriesSumNode));
+
+    if (new_node == NULL) {
+        DEBUG_PRINT("Memory allocation failed");
+        exit(EXIT_FAILURE);
+    }
+
+    new_node->calories_sum = calories_sum;
+    new_node->next = NULL;
+
+    return new_node;
+}
+
+void append_to_end_calories_sum_list(struct CaloriesSumNode** head, int calories_sum) {
+    struct CaloriesSumNode* new_node = create_calories_sum_node(calories_sum);
+
+    if (*head == NULL) {
+        // Is empty singly list
+        *head = new_node;
+        return;
+    }
+
+    struct CaloriesSumNode* current = *head;
+    while (current->next != NULL) {
+        current = current->next; // Ensure at the end of the list
+    }
+
+    current->next = new_node;
+}
+
 int main() {
     DEBUG_PRINT("Advent of Code Day 1");
 
@@ -21,6 +57,8 @@ int main() {
 
     char line[16]; // Buffer to store each line
 
+    struct CaloriesSumNode* head = NULL;
+
     int idx_of_elf       = 0; // Index of elf being processed
     int calories_max     = 0; // Maximum calories sum found
     int elf_calories_sum = 0; // Calories sum for each Elf
@@ -35,6 +73,7 @@ int main() {
             // Done one Elf
             DEBUG_PRINT("Sum of calories for Elf #%d: %d", idx_of_elf, elf_calories_sum);
 
+            append_to_end_calories_sum_list(&head, elf_calories_sum);
             if (elf_calories_sum > calories_max) {
                 calories_max = elf_calories_sum;
                 DEBUG_PRINT("Found new maximum calories: %d", calories_max);
@@ -51,6 +90,19 @@ int main() {
     fclose(fp);
 
     DEBUG_PRINT("===> Maximum calories found: %d <===", calories_max);
+
+    struct CaloriesSumNode* current = head;
+    int idx = 0;
+    int max = 0;
+    while (current != NULL) {
+        DEBUG_PRINT("***Sum of calories for Elf #%d: %d", ++idx, current->calories_sum);
+        if (current->calories_sum > max) {
+            max = current->calories_sum;
+            DEBUG_PRINT("***Found new maximum calories: %d", max);
+        }
+        current = current->next;
+    }
+    DEBUG_PRINT("***> Maximum calories found: %d <***", max);
 
     return 0;
 }
