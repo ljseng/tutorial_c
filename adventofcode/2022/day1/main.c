49dea421b9b0fe9115aa26eab6af42026a232dfa
ljseng <sengleejun@gmail.com>
2023-10-01 04:07:35 +0800

    wip: handled now newline at eof using nested linked list

--- adventofcode/2022/day1/main.c
+++ adventofcode/2022/day1/main.c
@@ -4,27 +4,27 @@
 
 #include "../../../dbgprint/debug_print.h"
 
-struct CaloriesSumNode {
-    int calories_sum;
-    struct CaloriesSumNode* next;
+struct CaloriesNode {
+    int calories;
+    struct CaloriesNode* next;
 };
 
-struct CaloriesSumNode* create_calories_sum_node(int calories_sum) {
-    struct CaloriesSumNode* new_node =  (struct CaloriesSumNode*)malloc(sizeof(struct CaloriesSumNode));
+struct CaloriesNode* create_calories_node(int calories) {
+    struct CaloriesNode* new_node = (struct CaloriesNode*)malloc(sizeof(struct CaloriesNode));
 
     if (new_node == NULL) {
         DEBUG_PRINT("Memory allocation failed");
         exit(EXIT_FAILURE);
     }
 
-    new_node->calories_sum = calories_sum;
+    new_node->calories = calories;
     new_node->next = NULL;
 
     return new_node;
 }
 
-void append_to_end_calories_sum_list(struct CaloriesSumNode** head, int calories_sum) {
-    struct CaloriesSumNode* new_node = create_calories_sum_node(calories_sum);
+void append_to_end_calories_list(struct CaloriesNode** head, int calories) {
+    struct CaloriesNode* new_node = create_calories_node(calories);
 
     if (*head == NULL) {
         // Is empty singly list
@@ -32,7 +32,45 @@ void append_to_end_calories_sum_list(struct CaloriesSumNode** head, int calories
         return;
     }
 
-    struct CaloriesSumNode* current = *head;
+    struct CaloriesNode* current = *head;
+
+    while (current->next != NULL) {
+        current = current->next; // Ensure at the end of the list
+    }
+
+    current->next = new_node;
+}
+
+struct ElfNode {
+    struct CaloriesNode* calories;
+    struct ElfNode* next;
+};
+
+struct ElfNode* create_elf_node() {
+    struct ElfNode* new_node =  (struct ElfNode*)malloc(sizeof(struct ElfNode));
+
+    if (new_node == NULL) {
+        DEBUG_PRINT("Memory allocation failed");
+        exit(EXIT_FAILURE);
+    }
+
+    new_node->calories = NULL;
+    new_node->next = NULL;
+
+    return new_node;
+}
+
+void append_to_end_elf_list(struct ElfNode** head, struct CaloriesNode* calories) {
+    struct ElfNode* new_node = create_elf_node();
+    new_node->calories = calories;
+
+    if (*head == NULL) {
+        // Is empty singly list
+        *head = new_node;
+        return;
+    }
+
+    struct ElfNode* current = *head;
     while (current->next != NULL) {
         current = current->next; // Ensure at the end of the list
     }
@@ -57,52 +95,44 @@ int main() {
 
     char line[16]; // Buffer to store each line
 
-    struct CaloriesSumNode* head = NULL;
+    struct ElfNode* elfHead = NULL;
+    struct CaloriesNode* caloriesHead = NULL;
+
+    // Read and print each line until EOF
+    while (fgets(line, sizeof(line), fp) != NULL) {
+        append_to_end_elf_list(&elfHead, caloriesHead);
+
+        if (line[0] == '\n' || line[0] == '\0') {
+            // Done one Elf
+            caloriesHead = NULL;
+            continue;
+        }
+
+        append_to_end_calories_list(&caloriesHead, atoi(line));
+    }
+
+    fclose(fp);
 
     int idx_of_elf       = 0; // Index of elf being processed
     int calories_max     = 0; // Maximum calories sum found
     int elf_calories_sum = 0; // Calories sum for each Elf
 
-    // Read and print each line until EOF
-    while (fgets(line, sizeof(line), fp) != NULL) {
-        if (elf_calories_sum <= 0) {
-            DEBUG_PRINT("Summing calories for Elf #%d", ++idx_of_elf);
-        }
-
-        if (line[0] == '\n' || line[0] == '\0') {
-            // Done one Elf
-            DEBUG_PRINT("Sum of calories for Elf #%d: %d", idx_of_elf, elf_calories_sum);
-
-            append_to_end_calories_sum_list(&head, elf_calories_sum);
-            if (elf_calories_sum > calories_max) {
-                calories_max = elf_calories_sum;
-                DEBUG_PRINT("Found new maximum calories: %d", calories_max);
-            }
-
-            elf_calories_sum = 0;
-            continue;
-        }
-
-        elf_calories_sum += atoi(line);
-        DEBUG_PRINT("Iteration calories sum: %d", elf_calories_sum);
-    }
-
-    fclose(fp);
-
-    DEBUG_PRINT("===> Maximum calories found: %d <===", calories_max);
-
-    struct CaloriesSumNode* current = head;
-    int idx = 0;
-    int max = 0;
+    struct ElfNode* current = elfHead;
     while (current != NULL) {
-        DEBUG_PRINT("***Sum of calories for Elf #%d: %d", ++idx, current->calories_sum);
-        if (current->calories_sum > max) {
-            max = current->calories_sum;
-            DEBUG_PRINT("***Found new maximum calories: %d", max);
+        struct CaloriesNode* currentCalories = current->calories;
+        while (currentCalories != NULL) {
+            elf_calories_sum += currentCalories->calories;
+            currentCalories = currentCalories->next;
         }
+
+        if (elf_calories_sum > calories_max) {
+            calories_max = elf_calories_sum;
+        }
+
+        elf_calories_sum = 0;
         current = current->next;
     }
-    DEBUG_PRINT("***> Maximum calories found: %d <***", max);
 
+    DEBUG_PRINT("===> Maximum calories found: %d <===", calories_max);
     return 0;
 }
